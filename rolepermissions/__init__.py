a474ff545dd7684ea6fe9adb908f14226fba610a
Filipe Ximenes <filipeximenes@gmail.com>
2015-12-19 20:51:21 -0300

    fixing AppConfig

--- rolepermissions/__init__.py
+++ rolepermissions/__init__.py
@@ -8,3 +8,6 @@ dj_version = django.get_version()
 if StrictVersion(dj_version) < StrictVersion('1.7'):
     from rolepermissions.loader import load_roles_and_permissions
     load_roles_and_permissions()
+else:
+    default_app_config = 'rolepermissions.apps.RolePermissions'
+
--- rolepermissions/apps.py
+++ rolepermissions/apps.py
@@ -4,8 +4,8 @@ from django.apps import AppConfig
 from rolepermissions.loader import load_roles_and_permissions
 
 
-class MyAppConfig(AppConfig):
-    name = 'rolepermisions'
+class RolePermissions(AppConfig):
+    name = 'rolepermissions'
     verbose_name = "Django Role Permissions"
 
     def ready(self):
