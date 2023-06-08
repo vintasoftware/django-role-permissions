9c8b958d55d4d9a3fd98ce640ea89ffd28ab2d39
Dennys Azevedo <dennys.bd@gmail.com>
2023-06-08 20:09:26 +0000

    Fix converage config

--- setup.py
+++ setup.py
@@ -77,8 +77,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
